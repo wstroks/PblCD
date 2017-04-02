0b3ed55fa09279d9b066fc129d50019748c95287
Washington Pagotto Batista <wstroks@gmail.com>
2017-04-02 20:08:05 -0300

    projetos, perdi a versão final

---
+++ Treinando/1.1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+main(){
+	int A=0,B=0,X=0;
+	
+	
+	
+	scanf("%d",&A);
+	
+	scanf("%d",&B);
+	X=A+B;
+	
+	printf("X = %d", X);
+	system("\npause");
+	
+	
+}
---
+++ Treinando/1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+main(){
+	int A=0,B=0,X=0;
+	
+	
+	
+	scanf("%d",&A);
+	scanf("%d",&B);
+	X=A+B;
+	printf(" %d", X);
+	
+	
+}
